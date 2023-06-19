adb6750ec37c8b9648091dfda2abe64a518a891a
RodrigoProgram22 <rodrigoprograma22@gmail.com>
2023-06-19 14:32:18 -0300

    Calculadora funcionando.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'calculadora-angular';
+  valorMostrar: any = '';
+
+  sumNum(x: number) {
+    this.valorMostrar += x;
+  }
+  suma() {
+    this.valorMostrar += ' + ';
+  }
+  resta() {
+    this.valorMostrar += ' - ';
+  }
+  multi() {
+    this.valorMostrar += ' * ';
+  }
+  divid() {
+    this.valorMostrar += ' / ';
+  }
+  reset() {
+    this.valorMostrar = '';
+  }
+  resultado() {
+    try {
+      this.valorMostrar = eval(this.valorMostrar);
+    } catch (error) {
+      this.valorMostrar = 'Error!';
+    }
+  }
 }
